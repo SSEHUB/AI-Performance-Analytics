121af0cf695f4fc5ea8d8b565bb9c64914b43cec
elscha@sse.uni-hildesheim.de <elscha@sse.uni-hildesheim.de>
2025-02-07 08:14:15 +0000

    Further downloads

--- install.sh
+++ install.sh
@@ -8,19 +8,14 @@ downoadScript() {
     sudo chmod +x $1
 }
 
-
 echo "${TITLE}Installing Required Packages${NORMAL}"
 sudo apt -y install s-tui btop dmidecode lm-sensors tmux
 # sudo sensors-detect --auto
 
 echo "${TITLE}Downloading Scripts${NORMAL}"
 downloadScript cpu_monitor.sh
-
-curl -fsSL "${BASE_URL}/gpu_monitor.sh" -o gpu_monitor.sh
-sudo chmod +x gpu_monitor.sh
-
-curl -fsSL "${BASE_URL}/ram_monitor.sh" -o ram_monitor.sh
-sudo chmod +x ram_monitor.sh
+downloadScript gpu_monitor.sh
+downloadScript ram_monitor.sh
 
 curl -fsSL "${BASE_URL}/computeAvg.sh" -o computeAvg.sh
 sudo chmod +x computeAvg.sh
