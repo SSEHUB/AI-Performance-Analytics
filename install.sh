2173a5c949ef4b6f18ba0e91a1aae997c180ee78
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2025-02-07 07:54:36 +0000

    Download first script

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 TITLE="\033[31m"
 NORMAL="\033[0m"
+BASE_URL="https://raw.githubusercontent.com/SSEHUB/AI-Performance-Analytics/refs/heads/main/"
 
 echo -e "${TITLE}Installing Required Packages${NORMAL}"
 sudo apt -y install s-tui btop dmidecode lm-sensors tmux
 # sudo sensors-detect --auto
+
+echo -e "${TITLE}Downloading Scripts${NORMAL}"
+curl -fsSL "${BASE_URL}/cpu_monitor.sh"
+
